5c67b917ef4ba39cbbd65f9a10dfa90b1c7fc5c7
Andresv309 <andresv309@gmail.com>
2023-12-21 18:43:12 -0500

    Add global error handling using Controller approach

--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -8,13 +8,15 @@ var builder = WebApplication.CreateBuilder(args);
     builder.Services
         .AddApplication()
         .AddInfrastructure(builder.Configuration);
-    builder.Services.AddControllers(options => options.Filters.Add<ErrorHandlingFilterAttribute>());
+    // builder.Services.AddControllers(options => options.Filters.Add<ErrorHandlingFilterAttribute>());
+    builder.Services.AddControllers();
 }
 
 
 var app = builder.Build();
 {
     // app.UseMiddleware<ErrorHandlingMiddleware>();
+    app.UseExceptionHandler("/error");
     app.UseHttpsRedirection();
     app.MapControllers();
     app.Run();
