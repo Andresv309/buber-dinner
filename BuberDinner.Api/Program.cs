8e619f2ad49b85e7797d4adc6db52cd0447df9b7
Andresv309 <andresv309@gmail.com>
2023-12-20 20:29:22 -0500

    Add dependecy injection separated in projects

--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -1,5 +1,11 @@
+using BuberDinner.Application;
+using BuberDinner.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 {
+    builder.Services
+        .AddApplication()
+        .AddInfrastructure();
     builder.Services.AddControllers();
 }
 
