201505813ea8b297e65bc14cef58c7965b6998bb
Andresv309 <andresv309@gmail.com>
2023-12-21 01:26:16 -0500

    Add jwtTokenGenerator to api

--- BuberDinner.Application/Services/Authentication/AuthenticationService.cs
+++ BuberDinner.Application/Services/Authentication/AuthenticationService.cs
@@ -1,7 +1,16 @@
+using BuberDinner.Application.Common.Interfaces.Authentication;
+
 namespace BuberDinner.Application.Services.Authentication;
 
 public class AuthenticationService : IAuthenticationService
 {
+    private readonly IJwtTokenGenerator _jwtTokenGenerator;
+
+    public AuthenticationService(IJwtTokenGenerator jwtTokenGenerator)
+    {
+        _jwtTokenGenerator = jwtTokenGenerator;
+    }
+
     public AuthenticationResult Login(string email, string password)
     {
         return new AuthenticationResult(
@@ -15,12 +24,20 @@ public class AuthenticationService : IAuthenticationService
 
     public AuthenticationResult Register(string firstName, string lastName, string email, string password)
     {
+        // Check if user exists
+
+        // Create User
+        var userId = Guid.NewGuid();
+
+        // Create JWT
+        var token = _jwtTokenGenerator.GenerateToken(userId, firstName, lastName);
+
         return new AuthenticationResult(
-            Guid.NewGuid(),
+            userId,
             firstName,
             lastName,
             email,
-            "token"
+            token
         );
     }
 }
