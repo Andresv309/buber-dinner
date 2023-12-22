1f154ecbb288a904ddcd8410b51d80588ac1b72e
Andresv309 <andresv309@gmail.com>
2023-12-22 00:22:31 -0500

    Clean up project by deleting previous approaches

--- BuberDinner.Api/Controllers/ErrorsController.cs
+++ BuberDinner.Api/Controllers/ErrorsController.cs
@@ -12,12 +12,6 @@ public class ErrorsController : ControllerBase
     {
         Exception? exception = HttpContext.Features.Get<IExceptionHandlerFeature>()?.Error;
 
-        var (statusCode, message) = exception switch
-        {
-            IServiceException serviceException => ((int) serviceException.StatusCode, serviceException.ErrorMessage),
-            _ => (StatusCodes.Status500InternalServerError, "An unexpected error ocurred"),
-        };
-
-        return Problem(statusCode: statusCode, title: message);
+        return Problem();
     }
 }
--- BuberDinner.Application/Common/Errors/IServiceException.cs
+++
@@ -1,9 +0,0 @@
-using System.Net;
-
-namespace BuberDinner.Application.Common.Errors;
-
-public interface IServiceException
-{
-    public HttpStatusCode StatusCode { get; }
-    public string ErrorMessage { get; }
-}
