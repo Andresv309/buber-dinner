3dd9cce1df7da55067f04986b470c2de34218870
Andresv309 <andresv309@gmail.com>
2023-12-21 19:37:09 -0500

    Extend functionallity of ProblemDetails class by creatig a factory

--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -1,7 +1,9 @@
+using BuberDinner.Api.Errors;
 using BuberDinner.Api.Filters;
 using BuberDinner.Api.Middleware;
 using BuberDinner.Application;
 using BuberDinner.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 {
@@ -10,6 +12,8 @@ var builder = WebApplication.CreateBuilder(args);
         .AddInfrastructure(builder.Configuration);
     // builder.Services.AddControllers(options => options.Filters.Add<ErrorHandlingFilterAttribute>());
     builder.Services.AddControllers();
+
+    builder.Services.AddSingleton<ProblemDetailsFactory, BuberDinnerProblemDetailsFactory>();
 }
 
 
