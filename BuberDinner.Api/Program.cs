388852956d96544d0ae35015086acf2e867a4c7a
Andresv309 <andresv309@gmail.com>
2023-12-27 16:31:57 -0500

    Add authentication and authorization to the application, change jwtSettings injection mechanism and add validation scheme for jwt through bearer

--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -14,7 +14,10 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 {
     app.UseExceptionHandler("/error");
+
     app.UseHttpsRedirection();
+    app.UseAuthentication();
+    app.UseAuthorization();
     app.MapControllers();
     app.Run();
 }
