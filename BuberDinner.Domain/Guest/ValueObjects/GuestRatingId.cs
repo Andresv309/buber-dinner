7bc3c03b13a0f6861d8118d86af2ce375efea484
Andresv309 <andresv309@gmail.com>
2023-12-30 23:19:32 -0500

    Add guest aggregate

---
+++ BuberDinner.Domain/Guest/ValueObjects/GuestRatingId.cs
@@ -0,0 +1,28 @@
+using BuberDinner.Domain.Common.Models;
+
+namespace BuberDinner.Domain.Guest.ValueObjects;
+
+public sealed class GuestRatingId : ValueObject
+{
+    public Guid Value { get; }
+
+    private GuestRatingId(Guid value)
+    {
+        Value = value;
+    }
+
+    public static GuestRatingId CreateUnique()
+    {
+        return new(Guid.NewGuid());
+    }
+
+    public static GuestRatingId Create(Guid value)
+    {
+        return new(value);
+    }
+
+    public override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Value;
+    }
+}
