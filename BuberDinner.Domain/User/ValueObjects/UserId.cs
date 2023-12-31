a19afbc869cb83894bda893f8164e5c092627f96
Andresv309 <andresv309@gmail.com>
2023-12-30 23:20:32 -0500

    Add user aggregate

---
+++ BuberDinner.Domain/User/ValueObjects/UserId.cs
@@ -0,0 +1,28 @@
+using BuberDinner.Domain.Common.Models;
+
+namespace BuberDinner.Domain.User.ValueObjects;
+
+public sealed class UserId : ValueObject
+{
+    public Guid Value { get; }
+
+    private UserId(Guid value)
+    {
+        Value = value;
+    }
+
+    public static UserId CreateUnique()
+    {
+        return new(Guid.NewGuid());
+    }
+
+    public static UserId Create(Guid value)
+    {
+        return new(value);
+    }
+
+    public override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Value;
+    }
+}
