1754b3e161867cd5732833cf98f6baf3d9126025
Andresv309 <andresv309@gmail.com>
2024-01-12 12:13:22 -0500

    Move Repositories into an appropiate folder

--- BuberDinner.Infrastructure/Persistence/Repositories/UserRepository.cs
+++ BuberDinner.Infrastructure/Persistence/Repositories/UserRepository.cs
@@ -1,7 +1,7 @@
 using BuberDinner.Application.Common.Interfaces.Persistence;
 using BuberDinner.Domain.Entities;
 
-namespace BuberDinner.Infrastructure.Persistence;
+namespace BuberDinner.Infrastructure.Persistence.Repositories;
 
 public class UserRepository : IUserRepository
 {
