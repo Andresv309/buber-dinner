5c6159d936feebaedd47814243448d9c30e3ee25
Andresv309 <andresv309@gmail.com>
2024-01-11 23:15:29 -0500

    Add an in memory persistence provider for menu creation

--- BuberDinner.Infrastructure/DependencyInjection.cs
+++ BuberDinner.Infrastructure/DependencyInjection.cs
@@ -19,14 +19,26 @@ public static class DependencyInjection
         ConfigurationManager configuration
     )
     {
-        services.AddAuth(configuration);
+        services
+            .AddAuth(configuration)
+            .AddPersistence();
 
         services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
         
+        return services;
+    }
+
+    public static IServiceCollection AddPersistence(
+        this IServiceCollection services
+    )
+    {
         services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<IMenuRepository, MenuRepository>();
 
         return services;
     }
+
+
     public static IServiceCollection AddAuth(
         this IServiceCollection services,
         ConfigurationManager configuration
