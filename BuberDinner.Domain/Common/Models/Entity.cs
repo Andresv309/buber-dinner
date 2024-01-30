8ab134a1623ced64c387936aa4d23ffc2dc809a3
Andresv309 <andresv309@gmail.com>
2024-01-29 22:33:18 -0500

    Add basic structure for creating domain events for application

--- BuberDinner.Domain/Common/Models/Entity.cs
+++ BuberDinner.Domain/Common/Models/Entity.cs
@@ -1,9 +1,13 @@
 namespace BuberDinner.Domain.Common.Models;
 
-public abstract class Entity<TId> : IEquatable<Entity<TId>>
+public abstract class Entity<TId> : IEquatable<Entity<TId>>, IHasDomainEvents
     where TId : notnull
 {
+    private readonly List<IDomainEvent> _domainEvents = new();
+
     public TId Id { get; protected set; }
+
+    public IReadOnlyList<IDomainEvent> DomainEvents => _domainEvents.AsReadOnly();
 
     protected Entity(TId id)
     {
@@ -35,7 +39,17 @@ public abstract class Entity<TId> : IEquatable<Entity<TId>>
         return Equals((object?) other);
     }
 
-    #pragma warning disable CS8618
+    public void AddDomainEvent(IDomainEvent domainEvent)
+    {
+        _domainEvents.Add(domainEvent);
+    }
+
+    public void ClearDomainEvents()
+    {
+        _domainEvents.Clear();
+    }
+
+#pragma warning disable CS8618
     protected Entity()
     {
         // Required by EF Core
